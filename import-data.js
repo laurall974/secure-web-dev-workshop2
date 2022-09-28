4c6a63ff979e7a375b5eae710c90089de7356661
Llinares Laura <llinareslaura@MBP-de-Llinares.lan>
2022-09-28 18:20:47 +0200

    TD2

--- import-data.js
+++ import-data.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 // module dotenv = env en mémoire, config() va chercher .env
 //console.log(process.env)
 
-//import mongoose from 'mongoose';
-const mongoose = require('mongoose')
+const mongoose =  require('mongoose')
 const { Schema } = mongoose;
 
-mongoose.connect(process.env.MONGO_URI).then(() => {console.log('Connected!')
-    console.log('Toto')}) // promesse sur JS
+// on connecte a la bdd mongodb on recupere les credentials dans notre fichier .env
+mongoose.connect(process.env.MONGO_URI).then(() => {console.log('Connected!')}) //autoriser l'adresse IP de chez moi
 // .connect() est asynchrone
 
+// on définit le schema mongoose
 const filmSchema = new Schema({
 
     filmType: String,
@@ -34,3 +34,20 @@ const Location = mongoose.model('Location',filmSchema)
 
 //création du modele locations
 const maPremiereLocation = new Location({filmType:"Horror"})
+
+function addFilmingLocation(liste){
+    let film = new Location();
+    film.filmType = liste.fields.type_tournage;
+    film.filmProducerName = liste.fields.nom_producteur;
+    film.endDate = liste.fields.date_fin;
+    film.filmName = liste.fields.nom_tournage;
+    film.district = liste.fields.ardt_lieu;
+    film.geolocation.coordinates = liste.fields.geo_shape.coordinates;
+    film.geolocation.type = liste.fields.geo_shape.type;
+    film.sourceLocationId = liste.fields.id_lieu;
+    film.filmDirectorName = liste.fields.nom_realisateur;
+    film.address = liste.fields.adresse_lieu;
+    film.startDate = liste.fields.date_debut;
+    film.year = liste.fields.annee_tournage;
+
+}
