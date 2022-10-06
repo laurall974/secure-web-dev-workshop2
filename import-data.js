2bd9924bfbcc9d73ccb034973616a8595f2a54a0
Llinares Laura <llinareslaura@MBP-de-Llinares.lan>
2022-10-06 20:34:40 +0200

    TD2

--- import-data.js
+++ import-data.js
@@ -6,7 +6,9 @@ const mongoose =  require('mongoose')
 const { Schema } = mongoose;
 
 // on connecte a la bdd mongodb on recupere les credentials dans notre fichier .env
-mongoose.connect(process.env.MONGO_URI).then(() => {console.log('Connected!')}) //autoriser l'adresse IP de chez moi
+mongoose.connect(process.env.MONGO_URI).then(() => {console.log('Connected!')
+    getFilmingLocationById('2020-404');
+}) //autoriser l'adresse IP de chez moi
 // .connect() est asynchrone
 
 // on définit le schema mongoose
@@ -33,21 +35,40 @@ const filmSchema = new Schema({
 const Location = mongoose.model('Location',filmSchema)
 
 //création du modele locations
-const maPremiereLocation = new Location({filmType:"Horror"})
+//const maPremiereLocation = new Location({filmType:"Horror"})
+//maPremiereLocation.save().then()
 
-function addFilmingLocation(liste){
-    let film = new Location();
-    film.filmType = liste.fields.type_tournage;
-    film.filmProducerName = liste.fields.nom_producteur;
-    film.endDate = liste.fields.date_fin;
-    film.filmName = liste.fields.nom_tournage;
-    film.district = liste.fields.ardt_lieu;
-    film.geolocation.coordinates = liste.fields.geo_shape.coordinates;
-    film.geolocation.type = liste.fields.geo_shape.type;
-    film.sourceLocationId = liste.fields.id_lieu;
-    film.filmDirectorName = liste.fields.nom_realisateur;
-    film.address = liste.fields.adresse_lieu;
-    film.startDate = liste.fields.date_debut;
-    film.year = liste.fields.annee_tournage;
+async function addFilmingLocation(filmList){
 
+    for (const elt of filmList) {
+        let film = new Location();
+        film.filmType = elt.fields.type_tournage;
+        film.filmProducerName = elt.fields.nom_producteur;
+        film.endDate = elt.fields.date_fin;
+        film.filmName = elt.fields.nom_tournage;
+        film.district = elt.fields.ardt_lieu;
+        film.coordinates = elt.fields.geo_point_2d;
+        film.type = elt.fields.geo_shape.type;
+        film.sourceLocationId = elt.fields.id_lieu;
+        film.filmDirectorName = elt.fields.nom_realisateur;
+        film.address = elt.fields.adresse_lieu;
+        film.startDate = elt.fields.date_debut;
+        film.year = elt.fields.annee_tournage;
+        await film.save()
+
+    }
+
+}
+const filmingLocations = require('../secure-web-dev-workshop1/lieux-de-tournage-a-paris.json')
+//addFilmingLocation(filmingLocations);
+
+async function getFilmingLocationById(id){
+    const query = await Location.findOne({'sourceLocationId':id})
+    console.log(query);
+}
+
+
+function getFilmingLocationByFilmName (filmName){
+    const query =  Location.findOne({'filmName':filmName});
+    return query;
 }
