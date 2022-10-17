ea1be6f05b948303dcdeecd33666f2bbf0ea0b1d
Llinares Laura <llinareslaura@MBP-de-Llinares.lan>
2022-10-17 18:39:35 +0200

    TD2

--- import-data.js
+++ import-data.js
@@ -7,8 +7,13 @@ const { Schema } = mongoose;
 
 // on connecte a la bdd mongodb on recupere les credentials dans notre fichier .env
 mongoose.connect(process.env.MONGO_URI).then(() => {console.log('Connected!')
-    getFilmingLocationById('2020-404');
-}) //autoriser l'adresse IP de chez moi
+    getFilmingLocationById('2019-1611');
+    deleteLocationById('32'); //test : ne marche pas ok
+    deleteLocationById('633f19972cd2549596ba3922'); // ok !!
+    //addLocation('Horror','Laura MONGO','2018','Laura le retour','75020','2019-456','Roch Moreau','ESILV','2018-11-23');
+
+})
+
 // .connect() est asynchrone
 
 // on définit le schema mongoose
@@ -62,13 +67,54 @@ async function addFilmingLocation(filmList){
 const filmingLocations = require('../secure-web-dev-workshop1/lieux-de-tournage-a-paris.json')
 //addFilmingLocation(filmingLocations);
 
+//const maPremiereLocation = new Location({filmType:"Horror"})
+//maPremiereLocation.save().then()
+/** Question 9 :
+ * Write a function to query one Location by its ID
+ */
 async function getFilmingLocationById(id){
     const query = await Location.findOne({'sourceLocationId':id})
     console.log(query);
 }
 
+// on teste les fonctions plus haut dans le haut donnect pour par avoir de problème
+// si la fonction s'éxécute alors que la ocnnexion n'est pas encore établie
 
+/**Question 10 :
+ * Write a function to query all Locations for a given filmName
+ */
 function getFilmingLocationByFilmName (filmName){
     const query =  Location.findOne({'filmName':filmName});
     return query;
+}
+
+/**Question 11 :
+ * Write a function to delete a Location by its ID
+ */
+async function deleteLocationById(id){
+    try {
+        const query = await Location.deleteOne({'_id':id})
+        console.log(query);
+    } catch (error) {
+        //console.error(error);
+        console.error('ID does not exist');
+    }
+
+}
+
+/** Question 12:
+ * Write a function to add a Location
+ */
+function addLocation(filmType, filmProducerName,endDate,filmName, district, sourceLocationId,filmDirectorName,address,startDate,year){
+    const maPremiereLocation = new Location({filmType:filmType, filmProducerName:filmProducerName,endDate:endDate,filmName:filmName,district:district,sourceLocationId:sourceLocationId,filmDirectorName:filmDirectorName,address:address,startDate:startDate,year:year})
+    maPremiereLocation.save().then()
+    console.log('Location created');
+}
+
+/** Question 13 :
+ * Write a function to update a Location
+ */
+
+function updateLocation(){
+
 }
